d6dcd26398766bce08bb34cbc28f1d90b82ff775
jobin <jobinjose.in@gmail.com>
2022-12-06 11:01:39 +0530

    add course

--- src/main/java/com/nest/CourseApp/controller/CourseController.java
+++ src/main/java/com/nest/CourseApp/controller/CourseController.java
@@ -1,6 +1,9 @@
 package com.nest.CourseApp.controller;
 
+import com.nest.CourseApp.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +11,16 @@ public class CourseController {
 
     @GetMapping("/")
     public String HomePage(){
+        return "Welcome to CourseApp";
+    }
+
+    @PostMapping(path="/add", consumes = "application/json", produces = "application/json")
+    public String AddCourse(@RequestBody Course c){
+        System.out.println(c.getTitle());
+        System.out.println(c.getDescription());
+        System.out.println(c.getVenue());
+        System.out.println(c.getDuration());
+        System.out.println(c.getDate());
         return "Welcome to CourseApp";
     }
 
